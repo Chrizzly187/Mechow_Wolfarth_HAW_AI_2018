78068cf378bfb322d64ee4491eacf31030acf0ba
Chrizzly187 <Chrizzly187@hotmail.de>
2018-05-01 15:18:49 +0200

    POST Db test v1.5

--- src/main/java/account/AccountController.java
+++ src/main/java/account/AccountController.java
@@ -35,15 +35,11 @@ public class AccountController {
 	@PostMapping("/account")
 	@ResponseBody
 	public String registerAccount(@Valid @ModelAttribute("Account")Account account, Map<String, Object> model) {
-		System.out.println(account.getUserName());
-		System.out.println(account.getUserMail());
-		System.out.println(account.getPassword());
-		
 		
 	    try (Connection connection = dataSource.getConnection()) {
 	        Statement stmt = connection.createStatement();
 	        stmt.executeUpdate("CREATE TABLE IF NOT EXISTS users (userName varchar(255), userMail varchar(255), password varchar(255))");
-	        stmt.executeUpdate("INSERT INTO users VALUES (account.getUserName(), account.getUserMail(), account.getPassword())");
+	        stmt.executeUpdate("INSERT INTO users VALUES ("+account.getUserName()+", "+account.getUserMail()+", "+account.getPassword()+")");
 	        ResultSet rs = stmt.executeQuery("SELECT userName FROM users");
 
 	        ArrayList<String> output = new ArrayList<String>();
